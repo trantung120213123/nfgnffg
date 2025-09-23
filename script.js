e7408ddfc72c2aa6f4dc4e3de6b5ce365b019fd0
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-09-23 10:47:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function savePaste() {
   }
 
   try {
-    const res = await fetch('https://josefa-unlean-evangelina.ngrok-free.dev/api/new', {
+    const res = await fetch('jnytjtyjt-production.up.railway.app/api/new', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ title, content })
@@ -72,4 +72,5 @@ function escapeHtml(s) {
   if (!s) return ''; 
   return s.replace(/[&<>"']/g, c => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c])); 
 }
+
 
